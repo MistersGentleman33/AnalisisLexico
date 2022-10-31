ca0185e432e80efa19fc050adfb95e2f9e1cb090
Axel Caballero <mistersgentleman@localhost.localdomain>
2022-10-30 21:39:48 -0600

    Actualizacion identificacion variables

--- AnalisisLexico.py
+++ AnalisisLexico.py
@@ -2,11 +2,20 @@ import sys
 import re
 
 
-def recognize_variables(file):
-    expression = re.compile("(int [a-zA-Z]+ = [0-9]+|float [a-zA-Z]+ = [0-9]+.[0-9]+"
-                            "|double [a-zA-Z]+ = [0-9]+.[0-9]+|char [a-zA-Z]+ = [\"|\'][a-zA-Z]+[\"|\'])")
+def write_table(expression_int, expression_float, expression_double, expression_char):
+    passs
 
-    print(expression.findall(file))
+
+def recognize_variables(file):
+    # Expresiones regulares para cada variable
+
+    expression_int = re.compile("int [a-zA-Z]+ = [0-9]+|, [a-zA-Z]+ = [0-9]+")
+    expression_float = re.compile("float [a-zA-Z]+ = [0-9]+.[0-9]+|, [a-zA-Z]+ = [0-9]+.[0-9]+")
+    expression_double = re.compile("double [a-zA-Z]+ = [0-9]+.[0-9]+|, [a-zA-Z] = [0-9]+.[0-9]+")
+    expression_char = re.compile("char [a-zA-Z]+\[[0-9]+\] = [\"|\'][a-zA-Z]+[\"|\']"
+                                 "|, [a-zA-Z]+\[[0-9]+\] = [\"|\'][a-zA-Z]+[\"\']")
+
+    write_table(expression_int, expression_float, expression_double, expression_char)
 
 
 def read_file(input_text):  # Lectura del archivo, cada linea la guarda en una lista
@@ -23,7 +32,7 @@ def main():
     # errors = sys.argv[3]
 
     # Functions
-    file = read_file(input_text)
+    file = read_file(input_text) # Variable que contiene el archivo en string
     print("Esta es una prueba de lectura de archivo: \n {} \n".format(file))
     recognize_variables(file)
 
