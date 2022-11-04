814b96f245de0bd1d9602bea308fdac218dc02e6
root <root@localhost.localdomain>
2022-11-03 20:04:50 -0600

    Solo hay codigo en beta

--- prueba.py
+++ prueba.py
@@ -1,11 +1,30 @@
 import re
 
-expression = re.compile("(int [a-zA-Z]+ = [0-9]+|float [a-zA-Z]+ = [0-9]+.[0-9]+"
-                        "|double [a-zA-Z]+ = [0-9]+.[0-9]+|char [a-zA-Z]+ = [\"|\'][a-zA-Z]+[\"|\'])")
-text = """Hola hermano como has estado
-int asd = 10
-asdasdasd = 12e12e
-int a = 10 
-float asd = 102.123 asdas e10rj1209eu1092je 
-char asd = 'racquetball' """
-print(expression.findall(text))
+
+def expression():
+    expression = re.compile("(int [a-zA-Z]+ = [0-9]+|float [a-zA-Z]+ = [0-9]+.[0-9]+"
+                            "|double [a-zA-Z]+ = [0-9]+.[0-9]+|char [a-zA-Z]+ = [\"|\'][a-zA-Z]+[\"|\'])")
+    text = """ asdasd s"""
+    text2 = "asdasdasd"
+    expression2 = re.compile("(int [a-zA-Z]+ = [0-9]+|float [a-zA-Z]+ = [0-9]+.[0-9]+"
+                             "|double [a-zA-Z]+ = [0-9]+.[0-9]+|char [a-zA-Z]+ = [\"|\'][a-zA-Z]+[\"|\'])")
+
+    lista = expression.findall(text)
+    lista2 = expression2.findall(text)
+
+    if bool(l:
+        print("esta vacia")
+    else:
+        print("No esta vaia1")
+        print(lista)
+
+
+def main():
+    expression()
+
+
+#    file(expression)
+
+
+if __name__ == "__main__":
+    main()
