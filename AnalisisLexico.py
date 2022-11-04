d31c4c2eb870ed5a2af40a516ebb3977f8f4d52f
root <root@localhost.localdomain>
2022-11-03 20:03:59 -0600

    Actualizacion Expresion Regular y Tabla de simbolos

--- AnalisisLexico.py
+++ AnalisisLexico.py
@@ -2,20 +2,29 @@ import sys
 import re
 
 
-def write_table(expression_int, expression_float, expression_double, expression_char):
-    passs
+def write_table(list_int, list_float, list_double, list_char, table_name):
+    with open(table_name, "w") as file:
+        file.write("\tComponente Lexico\tLexema\tValor\n")
+
+    if not bool(list_int):
+        pass
+    else:
+        pass
 
 
-def recognize_variables(file):
-    # Expresiones regulares para cada variable
 
-    expression_int = re.compile("int [a-zA-Z]+ = [0-9]+|, [a-zA-Z]+ = [0-9]+")
-    expression_float = re.compile("float [a-zA-Z]+ = [0-9]+.[0-9]+|, [a-zA-Z]+ = [0-9]+.[0-9]+")
-    expression_double = re.compile("double [a-zA-Z]+ = [0-9]+.[0-9]+|, [a-zA-Z] = [0-9]+.[0-9]+")
-    expression_char = re.compile("char [a-zA-Z]+\[[0-9]+\] = [\"|\'][a-zA-Z]+[\"|\']"
-                                 "|, [a-zA-Z]+\[[0-9]+\] = [\"|\'][a-zA-Z]+[\"\']")
 
-    write_table(expression_int, expression_float, expression_double, expression_char)
+
+def recognize_variables(file):    # Expresiones regulares para cada variable
+
+    expression_int = re.compile("int .[^!#$%&/()=?^`°|]+ = [0-9]+|, [a-zA-Z]+ = [0-9]+")
+    expression_float = re.compile("float .[^!#$%&/()=?^`°|]+ = [0-9]+.[0-9]+|, [a-zA-Z]+ = [0-9]+.[0-9]+")
+    expression_double = re.compile("double .[^!#$%&/()=?^`°|]+ = [0-9]+.[0-9]+|, [a-zA-Z] = [0-9]+.[0-9]+")
+    expression_char = re.compile("char .[^!#$%&/()=?^`°|]+\[[0-9]+] = [\"|\'][a-zA-Z]+[\"|\']"
+                                 "|, [a-zA-Z]+\[[0-9]+] = [\"|\'][a-zA-Z]+[\"\']")
+
+    return expression_int.findall(file), expression_float.findall(file), \
+           expression_double.findall(file), expression_char.findall(file)
 
 
 def read_file(input_text):  # Lectura del archivo, cada linea la guarda en una lista
@@ -28,13 +37,14 @@ def read_file(input_text):  # Lectura del archivo, cada linea la guarda en una l
 def main():
     # Variables Flags
     input_text = sys.argv[1]
-    # symbols_table = sys.argv[2]
+    table_name = sys.argv[2]
     # errors = sys.argv[3]
 
     # Functions
-    file = read_file(input_text) # Variable que contiene el archivo en string
-    print("Esta es una prueba de lectura de archivo: \n {} \n".format(file))
-    recognize_variables(file)
+    file = read_file(input_text)  # Variable que contiene el archivo en string
+    #  print("Esta es una prueba de lectura de archivo: \n {} \n".format(file))
+    list_int, list_float, list_double, list_char = recognize_variables(file)
+    write_table(list_int, list_float, list_double, list_char, table_name)
 
 
 try:
@@ -45,4 +55,4 @@ except ValueError:
     print("\nInvalid Arguments: %s" % sys.argv[1])
 
 except:
-    print("\n[!] Use: python3 " + sys.argv[0] + " <code.c> " + " <symbols_table> " + "<errors>\n")
+    print("\n[!] Use: python3 " + sys.argv[0] + " <code.c> " + " <name_table>.txt " + "<errors>.txt\n")
